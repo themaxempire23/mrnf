284a7e021ecda240b6d61421e050917368466245
max <Phillemon.Haikali@transnamib.com.na>
2025-09-08 10:29:56 +0200

    node js learning started

---
+++ NodeJsApi/server.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http'); //in build http module wich allows me to create server and client
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200; // to indicate a succesful response
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('My Node Server');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
